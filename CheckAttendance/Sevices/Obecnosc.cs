aa5e8fcb061ce322be929c59576dc2c8cc5ebd83
Michał Grąbczewski <michalgrabczewskimail@gmail.com>
2022-11-25 15:47:16 +0100

    k

---
+++ CheckAttendance/Sevices/Obecnosc.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckAttendance.Sevices
+{
+    public class Obecnosc : IAttnendant
+    {
+        public string Name => "Michals";
+    }
+}
