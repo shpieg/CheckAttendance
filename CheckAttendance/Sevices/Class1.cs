a82a8bf38ec8686a6adb0d75a6e0aad32f153d9e
KB <karollos.bozek@gmail.com>
2022-11-25 15:45:05 +0100

    Zadanie

---
+++ CheckAttendance/Sevices/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckAttendance.Sevices
+{
+    internal class Test1 : IAttnendant
+    {
+        public string Name => "KB";
+    }
+}
