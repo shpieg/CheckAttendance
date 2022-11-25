95918dd5ddbdbd588395f674093591c447856124
lukasz.rachwalik <Lukasz.Rachwalik@globallogic.com>
2022-11-25 14:56:31 +0100

    Add project files.

---
+++ CheckAttendance/Extensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckAttendance
+{
+    public static class Extensions
+    {
+        public static List<Type> GetAssainableTypes<T>(this Assembly assembly)
+        {
+            return assembly.GetAssainableTypes(typeof(T));
+        }
+
+        public static List<Type> GetAssainableTypes(this Assembly assembly, Type currentType)
+        {
+            var result = new List<Type>();
+            foreach(var t in assembly.DefinedTypes)
+            {
+                if(currentType.IsAssignableFrom(t) && currentType != t)
+                    result.Add(t);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ CheckAttendance/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CheckAttendance;
+using CheckAttendance.Sevices;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+Console.WriteLine("Attedance check!");
+
+var services = new ServiceCollection();
+
+Assembly.GetEntryAssembly().GetAssainableTypes<IAttnendant>().ForEach(t =>
+{
+    services.AddTransient(typeof(IAttnendant), t);
+});
+
+var builder = services.BuildServiceProvider();
+
+var attendants = builder.GetServices<IAttnendant>();
+
+foreach(var a in attendants)
+{
+    Console.WriteLine(a.Name);
+}
