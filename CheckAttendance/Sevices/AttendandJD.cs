3778c10b0b3ebe4f490065b7d5a317eba401c094
shpieg <shpieggit@gmail.com>
2022-11-28 20:05:29 +0100

    Add new attendant

---
+++ CheckAttendance/Sevices/AttendandJD.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckAttendance.Sevices
+{
+    public class AttendandJD : IAttnendant
+    {
+        public string Name => "jny doo"
+    }
+}
